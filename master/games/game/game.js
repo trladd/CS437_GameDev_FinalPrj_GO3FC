7ad1fb7aa2e17ccc08b6905d316695bcbff9105d
trladd <trladd@iupui.edu>
2017-12-05 01:10:09 -0500

    Modified data connection properties
    Modified hwo data sending works. This makes it so that sending data is more precise and clear as to what will happen while allowing many variations of actions to occurr

--- master/games/game/game.js
+++ master/games/game/game.js
@@ -5,59 +5,61 @@
  * 
  */
 
- function MiniGameName(conn, playerNumber){
+function MiniGameName(conn, playerNumber) {
 
     //Game Values
     this.conn = conn;
     this.playerNumber = playerNumber;
     this.gameName = "Mini Game Name";
     this.keepGoing = true;
-    this.gameResults = new GameResult(0,0,this.gameName);
+    this.gameResults = new GameResult(0, 0, this.gameName);
 
     //Sets up what will happen when data is received
     conn.on('data', function (data) {
-        console.log('Received Exmaple Game Data: ', data);
+        console.log("Received to Game.js Datacontroller: " + data[0]);
+        switch (data[0]) {
+            //Various Cases for the first element of data being passed 
+            case "#sendID_1":
+                console.log("Interpreted received data as #send_id");
+                break;
+
+            case "gamejsGeneralData":
+                console.log("Received general data for gamejs:\n" + data[1]);
+                break;
+        }
     });
 
 
-    this.gameLoop = function(){
+
+    this.gameLoop = function () {
         //This is where you will put your game loop code
         //Set this.keepGoing != true for the game loop to exit
         //Ensure that you either have been updating the game results object
         //or that you update the game results object before the final return 
         //statement in this.runGame function
         //BEGIN EXAMPLE
-        for(i = 0; i < 10; i++){
-            console.log("Running Example game loop #" +i);
-            this.conn.send("Testing connection in game\nThis message sent from player " + this.playerNumber);
-            this.gameResults.addPlayerOneScore(5);
-            this.gameResults.addPlayerTwoScore(7);
+        for (i = 0; i < 10; i++) {
+            console.log("Running Example game loop #" + i);
+            this.gameResults.playerOneScore += 5;
+            this.gameResults.playerTwoScore += 7;
         }
-        if(this.gameResults.getPlayerOneScore() > this.gameResults.getPlayerTwoScore()){
-            this.gameResults.setWinner(1);
-        }
-        else{
-            this.gameResults.setWinner(2);
+        if (this.gameResults.playerOneScore > this.gameResults.playerTwoScore) {
+            this.gameResults.winner = 1;
+        } else {
+            this.gameResults.winner = 2;
         }
         this.keepGoing = false;
 
         ///END EXAMPLE
     }
 
-    this.resetDataRead = function(){
-        this.conn.on('data', function (data) {
-            console.log("Received data as: " + data);
-        });
-    }
-
-    this.runGame = function(){
+    this.runGame = function () {
         console.log("Running Game" + this.gameName);
-        while(this.keepGoing){
+        while (this.keepGoing) {
             this.gameLoop();
         }
         console.log("Ending Game " + this.gameName);
-        this.resetDataRead();
         this.gameResults.setPlayerOneScore(76);
         return this.gameResults;
     }
- }
+}
